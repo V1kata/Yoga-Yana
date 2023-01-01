888f2f76a608f2201165e248df6fadddf5ed0b20
V1kata <viktor.burboran@gmail.com>
2023-01-01 11:23:00 +0200

    Commit with gitignore and other lib

--- src/api/lib.js
+++ src/api/lib.js
@@ -1,2 +1,2 @@
-export { default as page } from '../../node_modules/page/page.mjs';
-export { html, render, nothing } from '../../node_modules/lit-html/lit-html.js';
+export { default as page } from '../../allLib/page.mjs';
+export { html, render, nothing } from '../../allLib/lit-html.js';
